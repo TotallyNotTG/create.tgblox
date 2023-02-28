0240b205a942f96c4681d4061b9b2e578c8975b5
TotallyNotTG <118136211+TotallyNotTG@users.noreply.github.com>
2023-02-28 13:47:08 -0800

    Add files via upload

---
+++ talent/storage/chunks/a2392jd.j.javascript.js
@@ -0,0 +1,16 @@
+document.write('<nav class="top-nav">');
+document.write('        <a href="creations.html"><img src="storage/images/studio_white.svg" class="logo"></a>');
+document.write('        <a href="https://totallynottg.github.io/create.tgblox/docs/docs"><button class="pages">Home</button></a>');
+document.write('        <h2 class="creator-text">Creator</h2>');
+document.write('        <a href="https://totallynottg.github.io/create.tgblox/create/creations"><button class="pages">Dashboard</button></a>');
+document.write('        <a href="https://totallynottg.github.io/create.tgblox/docs/docs"><button class="pages">Documentation</button></a>');
+document.write('        <a href="https://totallynottg.github.io/create.tgblox/marketplace/models"><button class="pages">Marketplace</button></a>');
+document.write('        <a href="https://totallynottg.github.io/devforums.tgblox"><button class="pages">Forums</button></a>');
+document.write('    </nav>');
+document.write('    <div class="main">');
+document.write('        <div class="message">');
+document.write('            <h1 class="">403 Access Denied</h1>');
+document.write('            <a>The Ollieblox Talent Hub is a new feature currently offered to a limited group of users. Unfortunately, your account does not qualify for access at this time. Please check back periodically to see if we have opened up the Talent Hub to more users.</a>');
+document.write('        </div>');
+document.write('        <img src="https://create.roblox.com/talent/assets/common/unauthorized.png">');
+document.write('    </div>');
